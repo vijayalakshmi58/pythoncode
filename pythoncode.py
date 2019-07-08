b49f136fc39feb45da917ec095f3503ab30da3b4
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-07-08 11:20:52 +0530

    Update pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -1,6 +1,6 @@
 # pythoncode
 ch = input("Enter a character: ")
-if((ch>='a' and ch<= 'z') ):
+if((ch>='a' and ch<= 'z')):
     print(ch, "is an Alphabet")
 else:
     print(ch, "is not an Alphabet")
