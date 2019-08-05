7bf6f4315fccfaca35c4e9e166bf9821de11511d
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-08-05 14:16:44 +0530

    Create ben33.py

---
+++ ben33.py
@@ -0,0 +1,8 @@
+ch=input()
+if ch.isalpha():
+    if(ch=='A'or ch=='E'or ch=='I'or ch=='O'or ch=='U'or ch=='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u'):
+          print("Vowel")
+    else:
+          print("Consonant")
+else:
+          print("invalid")
